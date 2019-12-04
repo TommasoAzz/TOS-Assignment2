7ec00b64c131d47d66df6a38118b19944611282e
TommasoAzz <azzalintommaso@gmail.com>
2019-12-04 22:32:11 +0100

    fix a #2, veniva calcolato sconto del 50% su prodotto meno caro, non su panino meno caro

--- src/main/java/it/unipd/tos/SandwichShopManagerBill.java
+++ src/main/java/it/unipd/tos/SandwichShopManagerBill.java
@@ -5,6 +5,7 @@ package it.unipd.tos;
 
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Stream;
 
 import it.unipd.tos.business.TakeAwayBill;
 import it.unipd.tos.business.exception.TakeAwayBillException;
@@ -17,9 +18,11 @@ public class SandwichShopManagerBill implements TakeAwayBill {
         // Calcolo del totale dell'ordine
         double totale = itemsOrdered.stream().mapToDouble((item) -> item.getPrice()).sum();
 
-        // Sconto 50% panino meno caro se acquisto più di 5 panini (ItemType.Panino)
-        if (itemsOrdered.stream().filter((item) -> item.getItemType() == ItemType.Panino).count() > 5) {
-            double minPrice = itemsOrdered.stream().min(Comparator.comparing(MenuItem::getPrice)).get().getPrice();
+        // Sconto 50% panino meno caro se acquisto più di 5 panini
+        // restituisco stream di MenuItem t.c. ogni oggetto di MenuItem nello stream abbia getItemType() == ItemType.Panino
+        Stream<MenuItem> panini = itemsOrdered.stream().filter((item) -> item.getItemType() == ItemType.Panino).unordered();
+        if (panini.count() > 5) {
+            double minPrice = panini.min(Comparator.comparing(MenuItem::getPrice)).get().getPrice();
             return totale - (minPrice * 0.5);
         }
 
