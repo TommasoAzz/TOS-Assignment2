05503714414944d5c02fb86d0e149ce2274e1ba1
TommasoAzz <azzalintommaso@gmail.com>
2019-12-04 14:12:39 +0100

    aggiunta interfaccia TakeAwayBill

---
+++ src/main/java/it/unipd/tos/business/TakeAwayBill.java
@@ -0,0 +1,13 @@
+////////////////////////////////////////////////////////////////////
+// TOMMASO AZZALIN 1169740
+////////////////////////////////////////////////////////////////////
+package it.unipd.tos.business;
+
+import java.util.List;
+
+import it.unipd.tos.business.exception.TakeAwayBillException;
+import it.unipd.tos.model.MenuItem;
+
+public interface TakeAwayBill {
+    double getOrderPrice(List<MenuItem> itemsOrdered) throws TakeAwayBillException;
+}
