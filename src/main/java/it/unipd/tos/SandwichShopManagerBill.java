18ed2c48b4b1ee2241abe2f7f441672fb07d8ebe
TommasoAzz <azzalintommaso@gmail.com>
2019-12-04 22:23:45 +0100

    implementato #2 (senza test)

--- src/main/java/it/unipd/tos/SandwichShopManagerBill.java
+++ src/main/java/it/unipd/tos/SandwichShopManagerBill.java
@@ -3,6 +3,7 @@
 ////////////////////////////////////////////////////////////////////
 package it.unipd.tos;
 
+import java.util.Comparator;
 import java.util.List;
 
 import it.unipd.tos.business.TakeAwayBill;
@@ -13,6 +14,15 @@ import it.unipd.tos.model.MenuItem;
 public class SandwichShopManagerBill implements TakeAwayBill {
     @Override
     public double getOrderPrice(List<MenuItem> itemsOrdered) throws TakeAwayBillException {
-        return itemsOrdered.stream().mapToDouble((item) -> item.getPrice()).sum();
+        // Calcolo del totale dell'ordine
+        double totale = itemsOrdered.stream().mapToDouble((item) -> item.getPrice()).sum();
+
+        // Sconto 50% panino meno caro se acquisto più di 5 panini (ItemType.Panino)
+        if (itemsOrdered.stream().filter((item) -> item.getItemType() == ItemType.Panino).count() > 5) {
+            double minPrice = itemsOrdered.stream().min(Comparator.comparing(MenuItem::getPrice)).get().getPrice();
+            return totale - (minPrice * 0.5);
+        }
+
+        return totale;
     }
 }
