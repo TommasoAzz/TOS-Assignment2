33a0be8abb722f7948d824521447cf7803eea08c
TommasoAzz <azzalintommaso@gmail.com>
2019-12-05 00:10:54 +0100

    aggiunti test per #1, modificata getOrderPrice per supportare il test degli item null

--- src/main/java/it/unipd/tos/SandwichShopManagerBill.java
+++ src/main/java/it/unipd/tos/SandwichShopManagerBill.java
@@ -16,11 +16,11 @@ public class SandwichShopManagerBill implements TakeAwayBill {
     @Override
     public double getOrderPrice(List<MenuItem> itemsOrdered) throws TakeAwayBillException {
         // Calcolo del totale dell'ordine
-        double totale = itemsOrdered.stream().mapToDouble((item) -> item.getPrice()).sum();
+        double totale = itemsOrdered.stream().filter((item) -> item != null).mapToDouble((item) -> item.getPrice()).sum();
 
         // Sconto 50% panino meno caro se acquisto più di 5 panini
         // restituisco stream di MenuItem t.c. ogni oggetto di MenuItem nello stream abbia getItemType() == ItemType.Panino
-        Stream<MenuItem> panini = itemsOrdered.stream().filter((item) -> item.getItemType() == ItemType.Panino).unordered();
+        Stream<MenuItem> panini = itemsOrdered.stream().filter((item) -> item != null && item.getItemType() == ItemType.Panino).unordered();
         if (panini.count() > 5) {
             double minPrice = panini.min(Comparator.comparing(MenuItem::getPrice)).get().getPrice();
             return totale - (minPrice * 0.5);
---
+++ src/test/java/it/unipd/tos/SandwichShopManagerBillTest.java
@@ -0,0 +1,70 @@
+package it.unipd.tos;
+
+import it.unipd.tos.business.exception.TakeAwayBillException;
+import it.unipd.tos.model.ItemType;
+import it.unipd.tos.model.MenuItem;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SandwichShopManagerBillTest extends Assert {
+    private final SandwichShopManagerBill ssmb = new SandwichShopManagerBill();
+    private final List<MenuItem> itemsOrdered = new ArrayList<>();
+
+    // istanzie di esempio da usare per i test
+    // -- panini
+    private final MenuItem contadino = new MenuItem(ItemType.Panino, "Panino Contadino", 1.5); // panino meno costoso
+    private final MenuItem primavera  = new MenuItem(ItemType.Panino, "Panino Primavera", 5.0);
+    private final MenuItem vegetariano = new MenuItem(ItemType.Panino, "Panino Vegetariano", 6.0);
+    private final MenuItem giorno =  new MenuItem(ItemType.Panino, "Panino del giorno", 7.0); // panino più costoso
+    // -- bevande
+    private final MenuItem cola = new MenuItem(ItemType.Bevanda, "Cola", 2.5);
+    private final MenuItem acqua = new MenuItem(ItemType.Bevanda, "Acqua", 1.0);
+    // -- fritti
+    private final MenuItem arancini = new MenuItem(ItemType.Fritto, "Arancini", 1.5);
+    private final MenuItem oliveascolane = new MenuItem(ItemType.Fritto, "Olive Ascolane", 3.5);
+
+    @Before
+    public void emptyList() {
+        itemsOrdered.clear();
+    }
+
+    @Test
+    public void getOrderPrice_totalPrice_calculated() throws TakeAwayBillException {
+        itemsOrdered.addAll(Arrays.asList(
+            primavera, // 5
+            vegetariano, // 6
+            cola, // 2.5
+            arancini, // 1.5
+            oliveascolane, // 3.5
+            giorno, // 7.0
+            acqua, // 1.0
+            contadino // 1.5
+        )); // 3 panini, 2 bevande, 2 fritti, totale = 28
+
+        assertEquals(ssmb.getOrderPrice(itemsOrdered), 28, 0.0);
+    }
+
+    @Test
+    public void getOrderPrice_emptyList_totalEquals0() throws TakeAwayBillException {
+        assertEquals(ssmb.getOrderPrice(itemsOrdered), 0.0, 0.0);
+    }
+
+    @Test
+    public void getOrderPrice_NaNPrice_totalEqualsNaN() throws TakeAwayBillException {
+        itemsOrdered.add(new MenuItem(ItemType.Panino, "NaN Test", Double.NaN));
+        itemsOrdered.add(new MenuItem(ItemType.Panino, "NaN Test", Double.NaN));
+        assertEquals(ssmb.getOrderPrice(itemsOrdered), Double.NaN, 0.0);
+    }
+
+    @Test
+    public void getOrderPrice_nullItems_totalEquals0() throws TakeAwayBillException {
+        itemsOrdered.add(null);
+        assertEquals(ssmb.getOrderPrice(itemsOrdered), 0.0, 0.0);
+    }
+}
